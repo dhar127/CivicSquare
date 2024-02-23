b1c5f87a66e0e2bf20b16bd3bee6342a5f5bfba1
dhar127 <dhar22028.cs@rmkec.ac.in>
2024-02-23 18:57:09 +0530

    js

--- src/LoginSignUp.jsx
+++ src/LoginSignUp.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import user from './assets/user.png';
 import aadhar from './assets/aadhar.png';
 import otp from './assets/otp.png';
 import mobile from './assets/mobile.png';
 import './LoginSignUp.css';
 const LoginSignUp = () => {
-  
+  const[action,setAction]=useState("Login");
   return (
     <div className="container">
       <div className="header">
         <div className="text">
-        SignUp
+        {action}
         </div>
         <div className="underline"></div>
       </div>
       <div className="inputs">
+        {action==="Login"?<div></div>:
       <div className="input">
         <img src={user} alt="" align="center"/>
         <input type='text' placeholder='Name'/>
       </div>
+      }
       <div className="input">
         <img src={aadhar} alt=""/>
         <input type='text' placeholder='Aadhar Number'/>
@@ -32,12 +34,13 @@ const LoginSignUp = () => {
         <input type='password' placeholder='OTP'/>
       </div>
       </div>
-      <div className="forgot-password" align="center">Lost Password? <span>Click here</span></div>
+      {action==="SignUp"?<div></div>:<div className="forgot-password" align="center">Lost Password? <span>Click here</span></div>}
+      
       <div className="submit-container">
-        <div className="submit">
+        <div className={action==="Login"?"submit gray":"submit"} onClick={()=>setAction("SignUp")}>
           SignUp
         </div>
-        <div className="submit">
+        <div className={action==="SignUp"?"submit gray":"submit"} onClick={()=>setAction("Login")}>
           Login
         </div>
       </div>
