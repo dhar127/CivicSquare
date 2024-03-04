18ffc4355fb6c514ae28e0baee64e6889e126a82
AshCoder07 <aswinrr17@gmail.com>
2024-03-04 07:02:03 +0530

    made changes

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -31,8 +31,8 @@ export default function Homepage() {
             </div>
         </header>
         <div className='hero'>
-            <div className="herotxt">
-                <h1>CivicSquare</h1>
+            <div className="herotxt typewriter">
+                <h1>CivicSquare🌍</h1>
                 <p>Empowering Communities, Building Solutions Together</p>
             </div>
         </div>
@@ -47,8 +47,8 @@ export default function Homepage() {
                 <div className='about-content'>
                     <h1>About us</h1>
                     <p>The goal is to make public amenities easier to access and more efficient to ensure that people may easily interact with their local government.Our application CivicSquare offers a range of features designed for convenient and efficient access to public services.</p>
-                    <p>Access public services from anywhere, anytime, eliminating the need for physical visits to government offices.Minimize expenses for citizens by eliminating the need for travel and paperwork associated with accessing public services, ultimately saving time and money.</p>
-                    <p>By providing user-friendly tools and streamlined processes, our application CivicSquare aims to enhance efficiency and convenience, ultimately improving the overall experience of accessing public amenities.Empower's mission to simplify access to governmental services through the CivicSquare application addresses the frustration and inefficiency individuals face in interacting with local government. </p>
+                    {/* <p>Access public services from anywhere, anytime, eliminating the need for physical visits to government offices.Minimize expenses for citizens by eliminating the need for travel and paperwork associated with accessing public services, ultimately saving time and money.</p>
+                    <p>By providing user-friendly tools and streamlined processes, our application CivicSquare aims to enhance efficiency and convenience, ultimately improving the overall experience of accessing public amenities.Empower's mission to simplify access to governmental services through the CivicSquare application addresses the frustration and inefficiency individuals face in interacting with local government. </p> */}
                 </div>
             </div>
         </div>
@@ -60,8 +60,8 @@ export default function Homepage() {
                     {/* <h1>img</h1> */}
                     {/* <h1>img</h1> */}
                     <p>The goal is to make public amenities easier to access and more efficient to ensure that people may easily interact with their local government.Our application CivicSquare offers a range of features designed for convenient and efficient access to public services.</p>
-                    <p>Access public services from anywhere, anytime, eliminating the need for physical visits to government offices.Minimize expenses for citizens by eliminating the need for travel and paperwork associated with accessing public services, ultimately saving time and money.</p>
-                    <p>By providing user-friendly tools and streamlined processes, our application CivicSquare aims to enhance efficiency and convenience, ultimately improving the overall experience of accessing public amenities.Empower's mission to simplify access to governmental services through the CivicSquare application addresses the frustration and inefficiency individuals face in interacting with local government. </p>
+                    {/* <p>Access public services from anywhere, anytime, eliminating the need for physical visits to government offices.Minimize expenses for citizens by eliminating the need for travel and paperwork associated with accessing public services, ultimately saving time and money.</p>
+                    <p>By providing user-friendly tools and streamlined processes, our application CivicSquare aims to enhance efficiency and convenience, ultimately improving the overall experience of accessing public amenities.Empower's mission to simplify access to governmental services through the CivicSquare application addresses the frustration and inefficiency individuals face in interacting with local government. </p> */}
                 
                 </div>
                 <div className='services-img'>
